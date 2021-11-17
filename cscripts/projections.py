9f9e250ed38cb9d2e9b0ca14f97d2080f35e338e
LudekCizinsky <ludekcizinsky11@gmail.com>
2021-11-17 04:27:00 +0100

    Put together projection procedure using oop principles. It should be now easier to use its API. I integrated it within main ntb. Before making any conclusions, we should r-run the whole notebook and recompute all stuff.

--- cscripts/projections.py
+++ cscripts/projections.py
@@ -1,8 +1,8 @@
-# projections.py 
+# projections.py
 # script holding function to project bipartite network onto one class
 
 # last modified     : 16/11/21
-# author            : jonas-mika senghaas
+# author            : jonas-mika senghaas, ludek cizinsky
 
 import os
 import sys
@@ -11,73 +11,204 @@ from datetime import date
 import numpy as np
 import networkx as nx
 from tqdm import tqdm
+import scipy as sp
+from networkx.algorithms import bipartite
 
 local_path = os.path.dirname(os.path.realpath(__file__))
 if local_path not in sys.path:
     sys.path.append(local_path)
 
-#--- PROJECTIONS METHODS
-def simple_weight(G, u, v):
-    n_u, n_v = set(G[u]), set(G[v])
-    # len of set of intersection
-    return len(n_u & n_v)
 
-def jaccard(G, u, v):
-    n_u, n_v = set(G[u]), set(G[v])
-    # normalise len of set of intersection by union
-    return len(n_u & n_v) / len(n_u | n_v)
+class Projections:
 
-def hyperbolic(G, u, v):
-    common = set(G[u]) & set(G[v])
-    return sum([1/(len(set(G[node])) - 1) for node in common])
+    def __init__(self, G, nodes) -> None:
 
-def probs(G, u, v):
-    common = set(G[u]) & set(G[v])
-    return sum([1/(len(set(G[node]))*len(set(G[u]))) for node in common])
+        # User input
+        self.G = G
+        self.nodes = nodes
 
-def heats(G, u, v):
-    common = set(G[u]) & set(G[v])
-    return sum([1/(len(set(G[node]))*len(set(G[v]))) for node in common])
+        # Utility attributes
+        self.info = {
+            "simple_weight": {"func": self.simple_weight, "vec": False},
+            "simple_weight_vec": {"func": self.simple_weight_vec, "vec": True},
+            "jaccard": {"func": self.jaccard, "vec": False},
+            "hyperbolic": {"func": self.hyperbolic, "vec": False},
+            "probs": {"func": self.probs, "vec": False},
+            "heats": {"func": self.heats, "vec": False},
+            "hybrid": {"func": self.hybrid, "vec": False},
+            "euclidean": {"func": self.euclidean, "vec": True},
+            "euclidean_norm": {"func": self.euclidean_norm, "vec": True},
+            "pearson": {"func": self.pearson, "vec": True},
+            "cosine": {"func": self.cosine, "vec": True}
+        }
 
-def hybrid(G, u, v):
-    common = set(G[u]) & set(G[v])
-    return sum([1/(len(set(G[node]))*len(set(G[u]))*len(set(G[v]))) for node in common])
- 
-class VectorisedProjection:
-    def __init__(self, G, repos, users):
-        self.metric = None
-        self.adj = nx.algorithms.bipartite.matrix.biadjacency_matrix(G, repos, users)
-        self.repo_map = {repos[i]: i for i in range(len(repos))}
-    def project(self, G, u, v):
-        v_u = np.ravel(self.adj.getrow(0).todense().sum(axis=0))
-        v_v = np.ravel(self.adj.getrow(1).todense().sum(axis=0))
-        if self.metric == 'simple_weight':
-            return np.sum((v_u + v_v) == 2)
-        elif self.metric == 'euclidean':
-            return sp.spatial.distance.euclidean(v_u, v_v)
-        elif self.metric == 'normalised_euclidean':
-            return 1 / (np.sqrt(np.sum(np.power((v_u - v_v),2))) + 1)
-        elif self.metric == 'pearson':
-            return sp.stats.stats.pearsonr(v_u, v_v)[0] + 1
-        elif self.metric == 'cosine':
-            return 1 - sp.spatial.distance.cosine(v_u, v_v)
+    def simple_weight(self, u, v):
+        common = set(self.G[u]) & set(self.G[v])
+        return len(common)
+
+    def simple_weight_vec(self, v_u, v_v):
+        return np.sum((v_u + v_v) == 2)
+
+    def jaccard(self, u, v):
+        n_u, n_v = set(self.G[u]), set(self.G[v])
+        return len(n_u & n_v) / len(n_u | n_v)
+
+    def hyperbolic(self, u, v):
+        common = set(self.G[u]) & set(self.G[v])
+        return sum([1/(len(set(self.G[node])) - 1) for node in common])
+
+    def probs(self, u, v):
+        common = set(self.G[u]) & set(self.G[v])
+        return sum([1/(len(set(self.G[node]))*len(set(self.G[u]))) for node in common])
+
+    def heats(self, u, v):
+        common = set(self.G[u]) & set(self.G[v])
+        return sum([1/(len(set(self.G[node]))*len(set(self.G[v]))) for node in common])
+
+    def hybrid(self, u, v):
+        common = set(self.G[u]) & set(self.G[v])
+        return sum([1/(len(set(self.G[node]))*len(set(self.G[u]))*len(set(self.G[v]))) for node in common])
+
+    def euclidean(self, v_u, v_v):
+        return sp.spatial.distance.euclidean(v_u, v_v)
+
+    def euclidean_norm(self, v_u, v_v):
+        return 1 / (np.sqrt(np.sum(np.power((v_u - v_v), 2))) + 1)
+
+    def pearson(self, v_u, v_v):
+        return sp.stats.stats.pearsonr(v_u, v_v)[0] + 1
+
+    def cosine(self, v_u, v_v):
+        return 1 - sp.spatial.distance.cosine(v_u, v_v)
+
+    def node_to_vector(self, onto, adj, u):
+        index = onto.index(u)
+        return np.ravel(adj.getrow(index).todense().sum(axis=0))
+
+    def get_sparse_adj_matrix(self, rows, columns):
+        return bipartite.matrix.biadjacency_matrix(self.G, rows, columns)
+
+    def test_weight_func(self, name, info, u, v, which_n):
+
+        if name in self.info:
+
+            # Get the relevant projection info
+            P = info["func"]
+            is_vec = info["vec"]
+            onto = self.nodes[which_n]
+
+            # Compute sparse adjacency matrix if projection type requires it
+            if is_vec:
+                other = list(set(self.nodes.keys()) - set([which_n]))[0]
+                adj = self.get_sparse_adj_matrix(onto, self.nodes[other])
+            else:
+                adj = None
+
+            # Compute weight
+            if is_vec:
+                v_u = self.node_to_vector(onto, adj, u)
+                v_v = self.node_to_vector(onto, adj, v)
+                w = P(v_u, v_v)
+            else:
+                w = P(u, v)
+
+            # Delete Adjacency graph from memory since you no longer need it
+            del adj
+
+            return w
+
         else:
-            print("Please specify one of the following metrics: ['simple_weight', 'euclidean', 'normalised_euclidean', 'pearson', 'cosine'].")
+            raise SyntaxError(
+                f"Projection {name} does not exist within our projections. Please use one of these: {', '.join(list(self.info.keys()))}.")
 
+    def project(self, which_n, which_p, savepath):
+        """
+        Custom function to perform projections.
 
+        Algorithm
+        1A Use double for loop to iterate over all unique pairs of nodes and compute edge weight according to weight function.
+        1B Save each computed eddge to the given edgelist
 
-#--- PROJECTION
-def project(G, onto=):
-    # custom function to perform projections
+        :G: bi-partite graph
+        :which_n: str, which type of nodes to project graph onto
+        :which_p: str, which projection to use
+        :savepath: str, where to save the created edge list
+        :return: None
+        """
 
-    # algorithm
-    # 1. compute matrix of all neighbors for one class of nodes
-    # 2. use double for loop to iterate over all unique pairs of nodes (because
-    #    resulting graph is undirected) and compute edge weight according to 
-    #    weight function
-    #
-    # return projected graph including all node attributes () and computed edge weights
-    pass
+        if which_p in self.info:
+
+            # Get the relevant projection info
+            P = self.info[which_p]["func"]
+            is_vec = self.info[which_p]["vec"]
+            onto = self.nodes[which_n]
+
+            # Define constaint whether to accept computed weight or not
+            w_constraint = self.info[which_p].get("constr_w")
+            if not w_constraint:
+                def w_constraint(w): return w > 0
+
+            # Compute sparse adjacency matrix if projection type requires it
+            if is_vec:
+                other = list(set(self.nodes.keys()) - set([which_n]))[0]
+                adj = self.get_sparse_adj_matrix(onto, self.nodes[other])
+            else:
+                adj = None
+
+            # Overwrite the old content first with blank string
+            with open(savepath, 'w') as outfile:
+                outfile.write("")
+
+            # Append the computed edges into the savepath file
+            with open(savepath, 'a') as outfile:
+
+                # Add a header
+                outfile.write("src\ttrg\tweight\n")
+
+                # Iterate over all combinations of nodes which you want to project onto
+                for u in tqdm(onto):
+                    nbrs2 = {n for nbr in set(self.G[u])
+                             for n in self.G[nbr]} - {u}
+                    for v in nbrs2:
+
+                        # Compute weight for given edge (if any)
+                        if is_vec:
+                            v_u = self.node_to_vector(onto, adj, u)
+                            v_v = self.node_to_vector(onto, adj, v)
+                            w = P(v_u, v_v)
+                        else:
+                            w = P(u, v)
+
+                        # If it passes constraint, save it
+                        if w_constraint(w):
+                            # Save the edge (tab-separated)
+                            outfile.write(f"{u}\t{v}\t{w}\n")
+
+            # Delete Adjacency graph from memory since you no longer need it
+            del adj
+
+        else:
+            raise SyntaxError(
+                f"Projection {which_p} does not exist within our projections. Please use one of these: {', '.join(list(self.info.keys()))}.")
+
 
 if __name__ == '__main__':
-    pass
+
+    # Define G
+    G = nx.read_edgelist(f"../data/transformed/data.txt",
+                         delimiter=",", comments='#', create_using=nx.Graph)
+
+    # Define repos and users nodes
+    repos = [node for node in G.nodes() if node[0] == 'r']
+    users = [node for node in G.nodes() if node[0] == 'u']
+
+    # Initiate projections object
+    proj = Projections(G, nodes={"repos": repos, "users": users})
+
+    # Define u and v
+    u = "r1"
+    v = "r2"
+
+    # Test
+    proj.project(which_n="repos", which_p="simple_weight",
+                 savepath=f"../data/projections/edge_list_format/simple_weight.edges")
