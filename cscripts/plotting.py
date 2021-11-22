80cd7780f41e62c4350793d68a4b360b99fe0d0a
LudekCizinsky <ludekcizinsky11@gmail.com>
2021-11-22 14:23:00 +0100

    Fixed Mika's two bugs in the code in plotting.py. It was just reference errors (mispelled name of a function)

--- cscripts/plotting.py
+++ cscripts/plotting.py
@@ -1,9 +1,17 @@
-# plotting.py 
+# plotting.py
 # script holding function to analyse unipartite and bipartite networkx
 
 # last modified     : 16/11/21
 # author            : jonas-mika senghaas
 
+import metrics
+import powerlaw as pl
+from tqdm import tqdm
+import networkx as nx
+import seaborn as sns
+from matplotlib import pyplot as plt
+import pandas as pd
+import numpy as np
 import os
 import sys
 from time import time
@@ -14,36 +22,30 @@ local_path = os.path.dirname(os.path.realpath(__file__))
 if local_path not in sys.path:
     sys.path.append(local_path)
 
-import numpy as np
-import pandas as pd
-from matplotlib import pyplot as plt
-import seaborn as sns
-import networkx as nx
-from tqdm import tqdm
-import powerlaw as pl
-
-import metrics
 
 # global settings
 sns.set_style("darkgrid")
-sns.set(rc={"xtick.bottom" : True, "ytick.left" : True})
+sns.set(rc={"xtick.bottom": True, "ytick.left": True})
 
-def plot_single_degree_distribution(degrees, name='', ax=None, figsize=(5,5), scale='linear'):
+
+def plot_single_degree_distribution(degrees, name='', ax=None, figsize=(5, 5), scale='linear'):
     if ax == None:
         _, ax = plt.subplots(figsize)
 
     counter = Counter(degrees)
-    data = pd.DataFrame(list(counter.items()), columns = ("k", "count")).sort_values(by = "k")
+    data = pd.DataFrame(list(counter.items()), columns=(
+        "k", "count")).sort_values(by="k")
 
-    sns.scatterplot(data=data, x='k', y='count', 
-                    ax=ax, 
+    sns.scatterplot(data=data, x='k', y='count',
+                    ax=ax,
                     alpha=0.6)
 
-    ax.set_title(f"Degree Distribution of {name.title()} ({scale.title()}-Scale)", weight = "bold")
+    ax.set_title(
+        f"Degree Distribution of {name.title()} ({scale.title()}-Scale)", weight="bold")
     ax.set(xlabel='Degrees ($k$)', ylabel='Counts')
 
     if scale == 'log':
-        ax.set_xscale('log') 
+        ax.set_xscale('log')
         ax.set_yscale('log')
         ax.set_xlim(10**0*0.9, 10**5)
     elif scale != 'linear':
@@ -51,7 +53,8 @@ def plot_single_degree_distribution(degrees, name='', ax=None, figsize=(5,5), sc
 
     return ax
 
-def plot_degree_distribution(*degrees, names=None, scales='linear', figsize=(5,5)):
+
+def plot_degree_distribution(*degrees, names=None, scales='linear', figsize=(5, 5)):
     if names == None:
         names = ['Unnamed' for _ in range(len(degrees))]
     if type(scales) == str:
@@ -61,59 +64,63 @@ def plot_degree_distribution(*degrees, names=None, scales='linear', figsize=(5,5
     n_rows = len(degrees)
     n_cols = len(scales)
 
-    fig, axs = plt.subplots(nrows=n_rows, ncols=n_cols, figsize=(figsize[0]*n_cols, figsize[1]*n_rows), 
+    fig, axs = plt.subplots(nrows=n_rows, ncols=n_cols, figsize=(figsize[0]*n_cols, figsize[1]*n_rows),
                             constrained_layout=True)
 
     # formatting axes array
     axs = np.array(axs)
-    if n_rows > 1 and n_cols==1:
+    if n_rows > 1 and n_cols == 1:
         axs = axs.reshape(-1, 1)
     elif n_rows == 1 and n_cols > 1:
         axs = axs.reshape(1, -1)
     elif n_rows == 1 and n_cols == 1:
         axs = axs.reshape(1, 1)
 
-
     for i, degree, name in zip(range(n_rows), degrees, names):
         for j, s in zip(range(n_cols), scales):
-            fig = plot_single_degree_distribution(degree, name, 
-                                                  figsize=figsize, 
-                                                  ax=axs[i][j], 
+            fig = plot_single_degree_distribution(degree, name,
+                                                  figsize=figsize,
+                                                  ax=axs[i][j],
                                                   scale=s)
 
     return fig
 
-def plot_single_ccdf_degrees(degrees, fit=True, name='', ax=None, figsize=(5,5), scale='log'):
+
+def plot_single_ccdf_degrees(degrees, fit=True, name='', ax=None, figsize=(5, 5), scale='log'):
     if ax == None:
         _, ax = plt.subplots(figsize)
 
     counter = Counter(degrees)
-    data = pd.DataFrame(list(counter.items()), columns = ("k", "count")).sort_values(by = "k", ascending=False)
+    data = pd.DataFrame(list(counter.items()), columns=(
+        "k", "count")).sort_values(by="k", ascending=False)
 
     # computing ccdf
     data["cumsum"] = data["count"].cumsum()
     data["ccdf"] = data["cumsum"] / data["count"].sum()
-    data = data[["k", "ccdf"]].sort_values(by = "k")
+    data = data[["k", "ccdf"]].sort_values(by="k")
 
     # computing powerlaw fit
     if fit:
         results = pl.Fit(data["ccdf"], verbose=False)
-        data["fit"] = results.power_law.Kappa * (data["k"] ** -results.power_law.alpha)
+        data["fit"] = results.power_law.Kappa * \
+            (data["k"] ** -results.power_law.alpha)
 
     sns.lineplot(data=data, x='k', y='ccdf', ax=ax)
-    if fit: sns.lineplot(data=data, x='k', y='fit', ax=ax)
+    if fit:
+        sns.lineplot(data=data, x='k', y='fit', ax=ax)
 
-    ax.set_title(f"CCDF Degree Distribution of {name.title()} ({scale.title()}-Scale)", weight = "bold")
+    ax.set_title(
+        f"CCDF Degree Distribution of {name.title()} ({scale.title()}-Scale)", weight="bold")
     ax.set(xlabel='Degrees ($k$)', ylabel='CCDF (P(X>=k))')
 
-    ax.set_xscale('log') 
+    ax.set_xscale('log')
     ax.set_yscale('log')
     ax.set_xlim(10**0, data['k'].max())
 
     return ax
 
 
-def plot_ccdfs_degrees(*degrees, names=None, fit=True, figsize=(5,5)):
+def plot_ccdfs_degrees(*degrees, names=None, fit=True, figsize=(5, 5)):
     if names == None:
         names = ['Unnamed' for _ in range(len(degrees))]
     if type(fit) == bool:
@@ -123,43 +130,45 @@ def plot_ccdfs_degrees(*degrees, names=None, fit=True, figsize=(5,5)):
     n_rows = len(degrees)
     n_cols = len(fit)
 
-    fig, axs = plt.subplots(nrows=n_rows, ncols=n_cols, figsize=(figsize[0]*n_cols, figsize[1]*n_rows), 
+    fig, axs = plt.subplots(nrows=n_rows, ncols=n_cols, figsize=(figsize[0]*n_cols, figsize[1]*n_rows),
                             constrained_layout=True)
 
     # formatting axes array
     axs = np.array(axs)
-    if n_rows > 1 and n_cols==1:
+    if n_rows > 1 and n_cols == 1:
         axs = axs.reshape(-1, 1)
     elif n_rows == 1 and n_cols > 1:
         axs = axs.reshape(1, -1)
     elif n_rows == 1 and n_cols == 1:
         axs = axs.reshape(1, 1)
 
-
     for i, degree, name in zip(range(n_rows), degrees, names):
         for j, f in zip(range(n_cols), fit):
-            fig = plot_single_ccdf_degrees(degree, 
+            fig = plot_single_ccdf_degrees(degree,
                                            fit=f,
-                                           name=name, 
-                                           figsize=figsize, 
+                                           name=name,
+                                           figsize=figsize,
                                            ax=axs[i][j])
 
     return fig
 
-def plot_single_edge_weight_distribution(edge_weights, name='untitled', ax=None, log=True, figsize=(5,5)):
+
+def plot_single_edge_weight_distribution(edge_weights, name='untitled', ax=None, log=True, figsize=(5, 5)):
     if ax == None:
         fig, ax = plt.subplots(figsize=figsize)
-    sns.set_context(rc = {'patch.linewidth': 0.0})
+    sns.set_context(rc={'patch.linewidth': 0.0})
 
     sns.histplot(edge_weights, bins=20, kde=True, log_scale=log, ax=ax)
     ax.set(xlabel='Edge Weights', ylabel='Frequency')
-    ax.set(title=f"Edge Weight Distribution of {name.replace('_', ' ').title()}")
+    ax.set(
+        title=f"Edge Weight Distribution of {name.replace('_', ' ').title()}")
 
     return ax
 
-def plot_edge_weight_distributions(*edge_weights, names=None, log=True, figsize=(5,5)):
+
+def plot_edge_weight_distributions(*edge_weights, names=None, log=True, figsize=(5, 5)):
     if names == None:
-        names = ['Unnamed' for _ in range(len(degrees))]
+        names = ['Unnamed' for _ in range(len(edge_weights))]
     if type(log) == bool:
         log = [log]
 
@@ -167,22 +176,22 @@ def plot_edge_weight_distributions(*edge_weights, names=None, log=True, figsize=
     n_rows = len(edge_weights)
     n_cols = len(log)
 
-    fig, axs = plt.subplots(nrows=n_rows, ncols=n_cols, figsize=(figsize[0]*n_cols, figsize[1]*n_rows), constrained_layout=True)
+    fig, axs = plt.subplots(nrows=n_rows, ncols=n_cols, figsize=(
+        figsize[0]*n_cols, figsize[1]*n_rows), constrained_layout=True)
 
     # formatting axes array
     axs = np.array(axs)
-    if n_rows > 1 and n_cols==1:
+    if n_rows > 1 and n_cols == 1:
         axs = axs.reshape(-1, 1)
     elif n_rows == 1 and n_cols > 1:
         axs = axs.reshape(1, -1)
     elif n_rows == 1 and n_cols == 1:
         axs = axs.reshape(1, 1)
 
-
     for i, edge_weight, name in zip(range(n_rows), edge_weights, names):
         for j, l in zip(range(n_cols), log):
-            fig = plot_single_edge_weight_distribution(edge_weight, 
-                                                       name=name, 
+            fig = plot_single_edge_weight_distribution(edge_weight,
+                                                       name=name,
                                                        ax=axs[i][j],
                                                        log=l)
 
@@ -193,21 +202,25 @@ def generate_plots(G, name, filepath='.', create_path=True):
     if create_path:
         os.makedirs(filepath) if not os.path.exists(filepath) else None
 
-    names = ['degree_distribution', 'ccdf_degree_distribution', 'edge_weight_distribution']
-    funcs = [plot_degree_distribution, plot_ccdfs_degrees, plot_edge_weight_distribution]
-    
+    names = ['degree_distribution',
+             'ccdf_degree_distribution', 'edge_weight_distribution']
+    funcs = [plot_degree_distribution, plot_ccdfs_degrees,
+             plot_edge_weight_distributions]
+
     degrees = metrics.get_degrees(G)
     edge_weights = metrics.get_edge_weights(G)
 
     for func_name, func in zip(names, funcs):
-        if func_name == 'degree_distribution': 
+        if func_name == 'degree_distribution':
             fig = func(degrees, names=[name], scales=['linear', 'log'])
         elif func_name == 'ccdf_degree_distribution':
             fig = func(degrees, names=[name], fit=[False, True])
         elif func_name == 'edge_weight_distribution':
-            try: fig = func(edge_weights, name=name, log=True)
-            except: None
-        
+            try:
+                fig = func(edge_weights, name=name, log=True)
+            except:
+                None
+
         plt.savefig(f'{filepath}/{func_name}.jpg')
 
 
@@ -216,7 +229,7 @@ if __name__ == '__main__':
     start = time()
     G = nx.read_gpickle('../data/projections/pickle_format/jaccard.pickle')
     print('finished loading in ' + str(time() - start))
-    #G = nx.read_edgelist(f"../data/transformed/data.txt", delimiter=",", comments='#', create_using=nx.Graph)
+    # G = nx.read_edgelist(f"../data/transformed/data.txt", delimiter=",", comments='#', create_using=nx.Graph)
     #degrees = [G.degree(node) for node in G.nodes() if node[0]=='r']
     #G2 = nx.read_gpickle('../data/projections/pickle_format/heats.pickle')
 
@@ -227,9 +240,10 @@ if __name__ == '__main__':
     edge_weights = metrics.get_edge_weights(G)
     #edge_weights = [edge[-1]['weight'] for edge in G.edges(data=True)]
     print('finished computing edge weights in ' + str(time() - start))
-    #print(np.unique(edge_weights))
+    # print(np.unique(edge_weights))
 
     #fig = plot_degree_distribution(degrees, degrees2, names=['test1', 'test2'], scales=['linear', 'log'])
     #fig = plot_ccdfs_degrees(metrics.get_degrees(G), fit=[False, True])
-    fig = plot_edge_weight_distributions(edge_weights, names=['Jaccard'], log=[False, True])
+    fig = plot_edge_weight_distributions(
+        edge_weights, names=['Jaccard'], log=[False, True])
     plt.show()
