d126bc44168f6bd30e43c85848dcc7f1d01bb9b2
Jonas-Mika Senghaas <accounts@jonassenghaas.de>
2021-11-29 23:28:13 +0100

    Adjustments to recommendation algorithms - generalised to take the name of the edge attribute to weight by - accounted for singleton nodes that can occur as a result of backboning in recommendation

--- cscripts/recommendation.py
+++ cscripts/recommendation.py
@@ -1,11 +1,14 @@
+import numpy as np
+import pandas as pd
 import networkx as nx
 from tqdm import tqdm
 import json
 import os
 import math
 
+import backboning
 
-def naive_recommend(G, n=5):
+def naive_recommend(G, n=5, sort_by='weight'):
     """
     The NaiveRecommend algorithm makes recommendation sorely on the neighboring repositories
     in the final recommendation graph. For each node, we consider the n neighboring
@@ -19,13 +22,13 @@ def naive_recommend(G, n=5):
     recommendations = {}
     for node in tqdm(G.nodes()):
         sorted_neighbors = [x[0] for x in sorted(G[node].items(), 
-                            key=lambda x:x[1]['weight'], reverse=True)][:n]
+                            key=lambda x:x[1][sort_by], reverse=True)][:n]
         recommendations[node] = sorted_neighbors
 
     return recommendations
 
 
-def search_depth_recommend(G, n=5, search_depth='max'):
+def search_depth_recommend(G, n=5, sort_by='weight', search_depth='max'):
     """
     The SearchDepthRecommend improves the NaiveRecommend by also considering non-neighboring 
     repositories. It is based on the idea, that a low-weight neighboring repository is not 
@@ -44,7 +47,14 @@ def search_depth_recommend(G, n=5, search_depth='max'):
         path = [] 
         # start dijkstras with search depth
         current_search_depth = 1
-        max_neighbor = max(G[node].items(), key=lambda x:x[1]['weight'])
+        #print('current node', node)
+        #print('current neighbors', list(G[node].items()))
+        neighbors = G[node]
+        if len(neighbors) > 0:
+            max_neighbor = max(neighbors.items(), key=lambda x:x[1][sort_by])
+        else: 
+            recommendations[node] = []
+            continue
         visited = set(G[node])
         visited.add(node)
         path.append(max_neighbor)
@@ -64,12 +74,12 @@ def search_depth_recommend(G, n=5, search_depth='max'):
                 break
 
             # recompute max_neighbors
-            max_neighbor = max(max_neighbors.items(), key=lambda x:x[1]['weight'])
+            max_neighbor = max(max_neighbors.items(), key=lambda x:x[1][sort_by])
             path.append(max_neighbor)
             current_search_depth += 1
 
-        naive_recommend = [(key, val['weight']) for key, val in G[node].items()]
-        dijkstra_scores = list(_dijkstra_scores(path).items())
+        naive_recommend = [(key, val[sort_by]) for key, val in G[node].items()]
+        dijkstra_scores = list(_dijkstra_scores(path, sort_by=sort_by).items())
         all_recommend = naive_recommend + dijkstra_scores
 
         recommendations[node] = [x[0] for x in sorted(all_recommend, key=lambda x:x[1], reverse=True)][:n]
@@ -80,16 +90,19 @@ def write_recommendation(recommendations, metadata, filepath='.', name='untitled
     os.makedirs(filepath) if not os.path.exists(filepath) else None
 
     ans = {}
-    for key, val in recommendations:
-        key = metadata[key]['repo_name']
-        val = [metadata[v]['repo_name'] for v in val]
+    for key, val in recommendations.items():
+        try: key = metadata[key]['repo_name']
+        except: key = key
+
+        try: val = [metadata[v]['repo_name'] for v in val]
+        except: val = val
 
         ans[key] = val
 
     with open(f'{filepath}/{name}.json', 'w') as outfile:
-        json.dump(key, outfile)
+        json.dump(ans, outfile)
 
-def _dijkstra_scores(path, penalty='linear'):
+def _dijkstra_scores(path, sort_by='weight', penalty='linear'):
     scores = {}
     for i, node in enumerate(path):
         if i == 0:
@@ -97,7 +110,7 @@ def _dijkstra_scores(path, penalty='linear'):
         node,_  = node
         scores[node] = 0
         for j in range(1, i+2):
-            scores[node] += 1/j**2 * path[j-1][-1]['weight']
+            scores[node] += 1/j**2 * path[j-1][-1][sort_by]
         scores[node] /= i+1
     return scores
 
@@ -105,23 +118,41 @@ def _dijkstra_scores(path, penalty='linear'):
 
 if __name__ == '__main__':
     # generate test recommendation system
-    print('Start reading')
-    G = nx.read_weighted_edgelist('../data/projections/edge_list_format/simple_weight.edges', delimiter = '\t')
-    print('Done reading')
+    SIGNIF_THRS = 0.9
 
+    print('Loading backboned')
+    df = backboning.thresholding(
+                pd.read_csv(f"../data/backboning/noise_corrected/heats.csv"),
+                SIGNIF_THRS)
+    print('Finished loading\n')
+
+
+    print('Starting thresholding')
+    G = nx.convert_matrix.from_pandas_edgelist(df, source="src", target="trg", 
+                                               edge_attr=["nij", "score"], 
+                                               create_using=nx.DiGraph)
+    print('Finished thresholding\n')
+
+    print('Loading Metadata')
     with open('../data/transformed/metadata.json') as infile:
         metadata = json.load(infile)
+    print('Finished loading metadata\n')
 
     """
     weighted_edge_list = [
-            [1, 3, {'weight': 0.3}],
-            [1, 4, {'weight': 0.01}],
-            [2, 3, {'weight': 0.5}]
+            [1, 3, {'nij': 0.3, 'score': 1}],
+            [1, 4, {'nij': 0.01, 'score': 1}],
+            [2, 3, {'nij': 0.5, 'score': 1}]
             ]
 
     G = nx.from_edgelist(weighted_edge_list)
     """
-    write_recommendation(naive_recommend(G, n=1), metadata)
+
+    print('Starting building recommendation graph')
+    recommendation = search_depth_recommend(G, n=5, sort_by='nij')
+    print(recommendation)
+    write_recommendation(recommendation, metadata, name='search_depth')
+    print('done')
 
     #print('Naive Recommend: ', naive_recommend(G, n=2))
     #print('Search Depth Recommend: ', search_depth_recommend(G, search_depth=3))
